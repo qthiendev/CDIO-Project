7330d46abff235c0bf2e7b986545126c049b2ffe
TranThinh86 <130359567+TranThinh86@users.noreply.github.com>
2024-05-19 16:46:10 +0700

    new password

--- src/database/connectEEB.js
+++ src/database/connectEEB.js
@@ -1,30 +1,27 @@
 const sql = require("mssql");
 
 const config = {
-    driver: "msnodesqlv8",
-    user: "sa",
-    password: "Test@password",
-    server: "localhost",
-    database: "EEB",
-    options: {
-        port: 1433,
-        encrypt: true,
-        trustServerCertificate: true,
-    },
+  driver: "msnodesqlv8",
+  user: "sa",
+  password: "123456",
+  server: "localhost",
+  database: "EEB",
+  options: {
+    port: 1433,
+    encrypt: true,
+    trustServerCertificate: true,
+  },
 };
 
 const connectionEEB = new sql.ConnectionPool(config);
 
 connectionEEB
-    .connect()
-    .then((pool) => {
-        console.log("[SYSTEM] Connected to SQL Server | EEB.");
-    })
-    .catch((err) => {
-        console.error(
-            "[SYSTEM] Cannot connect to SQL Server | EEB: ",
-            err
-        );
-    });
+  .connect()
+  .then((pool) => {
+    console.log("[SYSTEM] Connected to SQL Server | EEB.");
+  })
+  .catch((err) => {
+    console.error("[SYSTEM] Cannot connect to SQL Server | EEB: ", err);
+  });
 
 module.exports = { connectionEEB };
